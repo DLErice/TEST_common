e88a244b6ea32b1dea434e4fe15bd8b3b4f6ac3e
DLErice <13110015696@163.com>
2016-10-27 14:26:32 +0800

    init

---
+++ TEST_Common/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TEST_Common
+//
+//  Created by 贺冬麟 on 2016/10/27.
+//  Copyright © 2016年 DL.Erice. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TEST_Common/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TEST_Common
+//
+//  Created by 贺冬麟 on 2016/10/27.
+//  Copyright © 2016年 DL.Erice. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
